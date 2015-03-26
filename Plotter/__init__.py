d76b1d94a742a99475437ee898fcc1dcb64becf5
amarini <andrea.marini@cern.ch>
2015-03-26 11:54:23 +0100

    going on writing the code

--- Plotter/__init__.py
+++ Plotter/__init__.py
@@ -45,9 +45,11 @@ class BaseDraw():
 		self.length = other.length
 		return self
 
-class DrawCollection():
+class Collection():
+	'''Store a collection of drawable objects'''
 	__init__():
-		self.objs=[]
+		self.objs={}
+		self.names=[]
 		self.cur=0
 	__del__():
 		pass
@@ -59,10 +61,21 @@ class DrawCollection():
 	__next__(self):
 		if self.cur < len(self) :
 			self.cur +=1 
-			return self.objs[self.cur - 1 ]
+			return self.objs[ self.names[ self.cur - 1  ] ]
 		else: 
 			raise StopIteration
 		pass
+	def Add(name,obj):
+		''' Add obj with name = name to the list '''
+		self.objs.append(obj)
+		self.names.append(name)
+		return self
+	def Remove(name):
+		del self.objs[name]
+		self.names[:] = [x for x in self.names and x != name ]
+		return self
+	def Get(name):
+		return self.objs[name]
 
 class Graph(BaseDraw):
 	__init__():
@@ -198,28 +211,173 @@ class Plotter:
 		self.cfg=config
 		self.canv_res=[800,800]
 		self.verbose=verbose
+		self.collection=Collection()
+		## make me configurable
+		self.labelsize=24
+		self.titlesize=28
+		self.entrysize=24
+		self.ratiofraction=0.2
+		self.axisHist=None
+		## sanity checks
 		if "base" not in cfg: raise TypeError
 		if "drawList" not in cfg: 
 			print>>sys.stderr, "--> You should indicate what you want to draw in drawList"
 			raise TypeError
-		for l in cfg["base"]["drawList"]:
-			if self.verbose >0: print "-> Adding Object '"+l+"' to the list to be drawn"
-			if l not in cfg: 
-				print>>sys.stderr, "Object '"+l+"' in drawList but not configured" 
-				raise NameError
+
 		if "legend" not in cfg: 
 			self.cfg["legend"] ={}
 			self.cfg["legend"]["draw"]="False"
+
+		for name in cfg["base"]["drawList"]:
+			if self.verbose >0: print "-> Adding Object '"+l+"' to the list to be drawn"
+			if name not in cfg: 
+				print>>sys.stderr, "Object '"+l+"' in drawList but not configured" 
+				raise NameError
+			##### Create obj
+			self.LoadObj( name )
 	__del__():
 		pass
-	def LoadTH1(name):
+	def LoadObj(name):
+		if self.verbose:
+			print "--- Loading Histo "+name+" ---"
+		if "file" not cfg[name]: 
+			print >>sys.stderr, "file not specified in cfg for histo", name
+			raise TypeError
+		if "obj"  not in cfg[name]:
+			print >>sys.stderr, "obj not specified in cfg for histo", name
+			raise TypeError
+		f = ROOT.TFile.Open(cfg[name]["file"] )
+		h = f.Get(cfg[name]["obj"])
+		self.collection.Add(name,h)
 		return self
 	# public 
 	def DrawCMS():
+		if "text" not in cfg["text"]: cfg["text"]["text"]= "CMS"
+		latex = ROOT.TLatex()
+		latex.SetNDC()
+		latex.SetFontSize(0.04)
+		latex.SetTextFont(42)
+		mytext="CMS"
+		if cfg["text"]["text"].lower() == "cms": mytext="#bf{CMS}"
+		if cfg["text"]["text"].lower() == "preliminary" : mytext="#bf{CMS},#scale[0.75]{ #it{Preliminary}}"
+		if cfg["text"]["text"].lower() == "unpublished" : mytext="#bf{CMS},#scale[0.75]{ #it{Unpublished}}"
+		if "position" not in cfg["text"] :  cfg["text"]["position"]="tl"
+
+		if cfg["text"]["position"].lower()[0] == "d":
+			if cfg["text"]["text"].lower() == "preliminary": mytext="#splitline{#bf{CMS}}{#scale[0.75]{#it{Preliminary}}}"
+			if cfg["text"]["text"].lower() == "unpublished": mytext="#splitline{#bf{CMS}}{#scale[0.75]{#it{Unpublished}}}"
+			if cfg["text"]["position"].lower() == "dtl": cfg["text"]["position"] = "tl"
+			if cfg["text"]["position"].lower() == "dtr": cfg["text"]["position"] = "tr"
+			if cfg["text"]["position"].lower() == "dbl": cfg["text"]["position"] = "bl"
+			if cfg["text"]["position"].lower() == "dbr": cfg["text"]["position"] = "br"
+
+		if cfg["text"]["position"].lower()=="tl":
+			latex.SetTextAlign(31)
+			x=self.canv.GetLeftMargin()+ 0.02
+			y=1-self.canv.GetTopMargin() -0.02
+		elif cfg["text"]["position"].lower()=="tr":
+			latex.SetTextAlign(33)
+			x=1-self.canv.GetRightMargin()- 0.02
+			y=1-self.canv.GetTopMargin() -0.02
+		elif cfg["text"]["position"].lower()=="bl":
+			latex.SetTextAlign(11)
+			x=self.canv.GetLeftMargin()+ 0.02
+			y=self.canv.GetBottomMargin() +0.02
+		elif cfg["text"]["position"].lower()=="br":
+			latex.SetTextAlign(13)
+			x=1-self.canv.GetRightMargin()- 0.02
+			y=self.canv.GetBottomMargin() +0.02
+
+		latex.DrawLatex(mytext,x,y)
+
+		#lumi always tr
+		x=1-self.canv.GetRightMargin()- 0.01
+		y=1-self.canv.GetTopMargin() +0.02
+		latex.SetFontSize(0.03)
+		latex.SetTextAlign(13)
+		if "lumi" not in cfg["text"]: cfg["text"]["lumi"]="19.7 fb^{-1} (8TeV)"
+		cfg["text"]["lumi"] = ParseString(cfg["text"]["lumi"]  )
+		latex.DrawLatex(x,y,cfg["text"]["lumi"])
+
+		if "extra" in cfg["text"]:
+			latex.SetTextAlign(11)
+			latex.SetTextSize(0.03)
+			x=float(cfg["text"]["extra"].split('!')[0] )
+			y=float(cfg["text"]["extra"].split('!')[1] )
+			mytext=cfg["text"]["extra"].split('!')[2]
+			mytext = ParseString(mytext)
+			latex.DrawLatex(x,y,mytext)
+
 		return self
+	def ParseStr(string):
+		''' Standard reparsing in the configfile for displayed text'''
+		r=re.sub('~',' ',string)
+		r=re.sub('@','#',string)
+		return r
+	def BoolKey(section,field,extraValues=False):
+		'''Parse cfg section and field to return a bool'''
+		if section not in self.cfg:
+			print >>sys.stderr, "section ", section,"not in cfg"
+			raise NameError
+		if field not in self.cfg[section]:
+			return False
+		if self.cfg[section][field].lower() == "false": return False
+		if self.cfg[section][field].lower() == "none": return False
+		if self.cfg[section][field].lower() == "no": return False
+		if self.cfg[section][field].lower() == "true": return True
+		if self.cfg[section][field].lower() == "yes": return True
+
+		if extraValues: return True
+		print>>sys.stderr, "Unrecognized bool option", self.cfg[section][field],"in",section+":"+field
+		raise NameError
 	def DrawLegend():
+		if self.cfg["legend"]["draw"].lower() == "false": return self
+		if self.cfg["legend"]["draw"].lower() == "no": return self
+
+		if self.cfg["legend"]["draw"].lower() != "yes" and
+		   self.cfg["legend"]["draw"].lower() != "true":
+			   print >>sys.stderr, "Unrecognized legend draw:",self.cfg["legend"]["draw"]
+			   raise TypeError
+
+		l=ROOT.TLegend()
+		l.SetFillStyle(0)
+		l.SetBorderSize(0)
+		if "header" in self.cfg["legend"]:
+			mytext= ParseStr(self.cfg["legend"]["header"])
+			e=l.SetHeader(mytext)
+			e.SetTextFont(43)
+			e.SetTextSize(self.entrysize)
+		if "legendList" not in self.cfg["legend"]: self.cfg["legend"]["legendList"]=""
+
+		for p1 in self.cfg["legend"]["legendList"]:
+			for name in p1.split(','):
+				if "label" in cfg[name] : mylabel=cfg[name]["label"]
+				else: mylabel = name
+
+				mylabel=ParseStr(mylabel)
+
+				if cfg[name]["style"].lower() == "marker": 
+					mytype = "P"
+					if BoolKey(name,"xerror"):
+						mytype += "L"
+					if BoolKey(name,"yerror"):
+						mytype += "E"
+				if cfg[name]["style"].lower() == "line": 
+					mytype = "L"
+				if cfg[name]["style"].lower() == "band": 
+					mytype = "F"
+				e=l.AddEntry(self.collection.Get(name),mylabel,mytype)
+				e.SetTextFont(43)
+				e.SetTextSize(self.entrysize)
+		l.Draw()
+
 		return self
-	def Draw():
+	def DrawObjects():
+		#TODO
+		for x in self.collection:
+			x.Draw()
+		return self
+	def DrawCanvas():
 		# CREATE CANVAS
 		if "canv" not in cfg["base"]:
 			self.canv=ROOT.TCanvas("canv","canv",800,800)
@@ -228,7 +386,102 @@ class Plotter:
 					int(self.cfg["base"]["canv"].split(',')[0]),
 					int(self.cfg["base"]["canv"].split(',')[1]),
 					)
+		if BoolKey("base","ratio",True):
+			self.pad1=ROOT.TPad("p1","p1",0,self.ratiofraction,1,1)
+			self.pad2=ROOT.TPad("p2","p2",0,0,1,self.ratiofraction)
+			self.pad1.Draw()
+			self.pad2.Draw()
+		else:
+			self.pad1=self.canv
+			self.pad2=None
+		self.pad1.cd()
+		## Draw AXIS from the first
+		base = cfg["base"]["drawList"].split(',')[0]	
+		if cfg[base]["type"].lower() == "th1d":
+			xmin = self.collection.Get(base).GetBinLowEdge(1)
+			n= self.collection.Get(base).GetNbinsX()
+			xmax = self.collection.Get(base).GetBinLowEdge(n+1)
+			ymin = self.collection.Get(base).GetMinimum()
+			ymax = self.collection.Get(base).GetMaximum()
+		if BoolKey("base","xRange",True):
+			xmin=float( cfg["base"]["xRange"].split(',')[0]  )
+			xmax=float( cfg["base"]["xRange"].split(',')[1]  )
+		if BoolKey("base","yRange",True):
+			ymin=float( cfg["base"]["yRange"].split(',')[0]  )
+			ymax=float( cfg["base"]["yRange"].split(',')[1]  )
+
+		axisHist= ROOT.TH2D("axis","axis",100,xmin,xmax,100,ymin,ymax)
+		axisHist.Draw("AXIS")
+		axisHist.Draw("AXIS X+Y+ SAME")
+		self.axisHist=axisHist
+		return self
+	def RedrawAxis():
+		axisHist.Draw("AXIS SAME")
+		axisHist.Draw("AXIS X+ Y+ SAME")
+		return self
+
+	def MakeRatio():
+		ratioBaseName=cfg["base"]["ratio"]
+		if ratioBaseName not in cfg: 
+			print >>sys.stderr,"Ratio Base name referred to a non configured histo",ratioBaseName
+			raise NameError
+		b=self.collection.Get(ratioBaseName).Clone(ratioBaseName + "_ratiobase")
+		## TODO:
+		## reset errors
+		## clone the collection in collratio
+		## perform the division
+		return self
+	def DrawRatio():
+		self.pad2.cd()
+		return self
+	def Draw():
+		Style().DrawCanvas().DrawObjects().DrawLegend().DrawCMS().RedrawAxis()
+		if BoolKey("base","ratio",True):
+			MakeRatio().DrawRatio()
 		return self
 	def Save():
+		for ext in self.cfg["base"]["format"].split(','):
+			self.canv.SaveAs( self.cfg["base"]["output"] + "."+ext)
 		return self
+	def Style():
+		''' Set Style TDR'''
+		ROOT.gStyle.SetOptTitle(0)
+		ROOT.gStyle.SetPadTopMargin(0.05)
+		ROOT.gStyle.SetPadBottomMargin(0.13)
+		ROOT.gStyle.SetPadLeftMargin(0.16)
+		ROOT.gStyle.SetPadRightMargin(0.02)
+		ROOT.gStyle.SetOptFile(0);
+		ROOT.gStyle.SetOptStat(0); 
+		ROOT.gStyle.SetOptDate(0);
+		ROOT.gStyle.SetOptFit(1);
+		ROOT.gStyle.SetFitFormat("5.4g");
+		ROOT.gStyle.SetFuncColor(2);
+		ROOT.gStyle.SetFuncStyle(1);
+		ROOT.gStyle.SetFuncWidth(1);
+		ROOT.gStyle.SetHistLineColor(1);
+		ROOT.gStyle.SetHistLineStyle(0);
+		ROOT.gStyle.SetHistLineWidth(1);
+		ROOT.gStyle.SetFrameBorderMode(0);
+		ROOT.gStyle.SetFrameBorderSize(1);
+		ROOT.gStyle.SetFrameFillColor(0);
+		ROOT.gStyle.SetFrameFillStyle(0);
+		ROOT.gStyle.SetFrameLineColor(1);
+		ROOT.gStyle.SetFrameLineStyle(1);
+		ROOT.gStyle.SetFrameLineWidth(1);
+		ROOT.gStyle.SetPadBorderMode(0);
+		#ROOSetPadBorderSize(Width_t size = 1);
+		ROOT.gStyle.SetPadColor(kWhite);
+		ROOT.gStyle.SetPadGridX(false);
+		ROOT.gStyle.SetPadGridY(false);
+		ROOT.gStyle.SetGridColor(0);
+		ROOT.gStyle.SetGridStyle(3);
+		ROOT.gStyle.SetGridWidth(1);
+		ROOT.gStyle.SetCanvasBorderMode(0);
+		ROOT.gStyle.SetCanvasColor(kWhite);
+		ROOT.gStyle.SetCanvasDefH(600); 
+		ROOT.gStyle.SetCanvasDefW(600); #Width of canvas
+		ROOT.gStyle.SetCanvasDefX(0);   #Position on screen
+		ROOT.gStyle.SetCanvasDefY(0);
+		return self
+
 
